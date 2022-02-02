9271ebedf380d7091c6bdc35f0fe1be34ada5d65
tommaso <codicem69@gmail.com>
2022-02-02 17:11:01 +0100

    aumentato lunghezza codice univoco dettagli fattura

--- packages/shipsteps/model/invoice_det.py
+++ packages/shipsteps/model/invoice_det.py
@@ -11,7 +11,7 @@ class Table(object):
         tbl.column('city', name_short='!![en]City place')
         tbl.column('vat', name_short='!![en]Vat number')
         tbl.column('cf', name_short='!![en]Fiscal code')
-        tbl.column('cod_univoco',size='6', name_short='!![en]Unique code')
+        tbl.column('cod_univoco',size='7', name_short='!![en]Unique code')
         tbl.column('pec', name_short='Email pec')
         tbl.formulaColumn('fullname',"""$rag_sociale ||' '|| coalesce($address, '') ||' '|| coalesce($cap,'') ||' '|| coalesce($city,'') || ' Vat: ' || coalesce($vat,'') || ' unique code: ' ||
                              coalesce($cod_univoco,'') || ' pec: ' || coalesce($pec,'') """ )
