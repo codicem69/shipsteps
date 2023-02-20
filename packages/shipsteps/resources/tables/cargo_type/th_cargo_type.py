d4b0817e35498d2e23724f401954ce4abb773ccd
tommaso <codicem69@gmail.com>
2023-02-20 11:08:00 +0100

    modifica th_cargo_type

--- packages/shipsteps/resources/tables/cargo_type/th_cargo_type.py
+++ packages/shipsteps/resources/tables/cargo_type/th_cargo_type.py
@@ -6,27 +6,16 @@ from gnr.core.gnrdecorator import public_method
 
 class View(BaseComponent):
 
-    def th_struct(self,struct):
+   def th_struct(self,struct):
         r = struct.view().rows()
-        r.fieldcell('parent_id')
-        r.fieldcell('descrizione')
         r.fieldcell('hierarchical_descrizione')
-        r.fieldcell('_parent_h_descrizione')
-        r.fieldcell('hierarchical_pkey')
-        r.fieldcell('_parent_h_pkey')
-        r.fieldcell('_h_count')
-        r.fieldcell('_parent_h_count')
-        r.fieldcell('_row_count')
-        r.fieldcell('df_fields')
-        r.fieldcell('df_fbcolumns')
-        r.fieldcell('df_custom_templates')
-        r.fieldcell('df_colswidth')
 
-    def th_order(self):
-        return 'parent_id'
 
-    def th_query(self):
-        return dict(column='parent_id', op='contains', val='')
+   def th_order(self):
+        return 'hierarchical_descrizione'
+
+   def th_query(self):
+        return dict(column='hierarchical_descrizione', op='contains', val='')
 
 
 
@@ -42,21 +31,7 @@ class Form(BaseComponent):
                                                                 margin='2px')
         form.htree.relatedTableHandler(th, dropOnRoot=False, inherited=True)
 
-       # pane = form.record
-       #fb = pane.formbuilder(cols=2, border_spacing='4px')
-       #fb.field('parent_id')
-       #fb.field('descrizione')
-       #fb.field('hierarchical_descrizione')
-       #fb.field('_parent_h_descrizione')
-       #fb.field('hierarchical_pkey')
-       #fb.field('_parent_h_pkey')
-       #fb.field('_h_count')
-       #fb.field('_parent_h_count')
-       #fb.field('_row_count')
-       #fb.field('df_fields')
-       #fb.field('df_fbcolumns')
-       #fb.field('df_custom_templates')
-       #fb.field('df_colswidth')
+
 
 
     def th_options(self):
