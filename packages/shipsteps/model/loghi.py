b842762e14548c6dd4d98d247dd7c5cb9669b0df
tommaso <codicem69@gmail.com>
2024-10-03 15:48:22 +0200

    inserito format='auto' nel model dei loghi in modo da visualizzare nella table direttamente l'immagine

--- packages/shipsteps/model/loghi.py
+++ packages/shipsteps/model/loghi.py
@@ -5,6 +5,6 @@ class Table(object):
         tbl=pkg.table('loghi', pkey='id', name_long='!![en]Logo', name_plural='!![en]Logos',
                                  caption_field='id')
         self.sysFields(tbl,counter=True)
-        tbl.column('logo_cp', dtype='P', name_short='!![en]Logo CP')
-        tbl.column('logo_imm', dtype='P', name_short='!![en]Logo Immigration')
+        tbl.column('logo_cp', dtype='P', name_short='!![en]Logo CP', format='auto')
+        tbl.column('logo_imm', dtype='P', name_short='!![en]Logo Immigration', format='auto')
         
