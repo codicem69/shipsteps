a7c36cf22ca43012e97a7c92dcc5cb3cd690c7b3
codicem69 <codicem69@gmail.com>
2025-03-13 14:40:20 +0100

    aggiunto la non duplicazione della checklist in arrival

--- packages/shipsteps/model/tasklist_check.py
+++ packages/shipsteps/model/tasklist_check.py
@@ -6,7 +6,7 @@ class Table(object):
         self.sysFields(tbl,counter=True)
 
         tbl.column('arrival_id',size='22', group='_', name_long='arrival_id'
-                    ).relation('arrival.id', relation_name='checktask_arrival', mode='foreignkey', onDelete='cascade')
+                    ).relation('arrival.id', relation_name='checktask_arrival', mode='foreignkey', onDelete='cascade',onDuplicate=False)
         tbl.column('done', dtype='B', name_short='!![en]Done')
         tbl.column('description', name_short='!![en]Description')
         tbl.column('note', name_short='Note')
